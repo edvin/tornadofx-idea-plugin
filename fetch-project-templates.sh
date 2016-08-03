c3af893c2c3df0a704b3ccaf45f64e072b89f0f8
edvin <es@syse.no>
2016-08-03 23:45:36 +0200

    Release 1.5.4.1

--- fetch-project-templates.sh
+++ fetch-project-templates.sh
@@ -2,11 +2,12 @@
 
 # Remember to build each template via Tools -> "Save project as template"
 
-#IDEA_PROJECT_TEMPLATES=~/Library/Preferences/IntelliJIdea2016.2/projectTemplates
-IDEA_PROJECT_TEMPLATES=~/.IntelliJIdea2016.2/config/projectTemplates
+IDEA_PROJECT_TEMPLATES=~/Library/Preferences/IntelliJIdea2016.2/projectTemplates
+#IDEA_PROJECT_TEMPLATES=~/.IntelliJIdea2016.2/config/projectTemplates
 
 PLUGIN_PROJECT_TEMPLATES=resources/projectTemplates
 
 cp ${IDEA_PROJECT_TEMPLATES}/tornadofx-maven-project.zip ${PLUGIN_PROJECT_TEMPLATES}
 cp ${IDEA_PROJECT_TEMPLATES}/tornadofx-maven-osgi-project.zip ${PLUGIN_PROJECT_TEMPLATES}
+cp ${IDEA_PROJECT_TEMPLATES}/tornadofx-maven-osgi-ds-project.zip ${PLUGIN_PROJECT_TEMPLATES}
 cp ${IDEA_PROJECT_TEMPLATES}/tornadofx-gradle-project.zip ${PLUGIN_PROJECT_TEMPLATES}
---
+++ template-projects/tornadofx-maven-osgi-project/src/main/kotlin/com/example/osgidemo/app/DashboardApp.kt
@@ -0,0 +1,6 @@
+package com.example.osgidemo.app
+
+import tornadofx.App
+import com.example.osgidemo.view.DashboardView
+
+class DashboardApp : App(DashboardView::class, Styles::class)
