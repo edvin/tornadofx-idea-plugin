db0d154edb29df07af591b7b59158632ab6a3a93
Edvin Syse <es@syse.no>
2016-09-06 10:52:00 +0200

    Generate properties from constructor parameters

--- src/no/tornado/tornadofx/idea/intentions/FXPropertyConverter.kt
+++ src/no/tornado/tornadofx/idea/intentions/FXPropertyConverter.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiWhiteSpace
 import com.intellij.psi.util.PsiTreeUtil
 import org.jetbrains.kotlin.idea.KotlinLanguage
 import org.jetbrains.kotlin.idea.core.quickfix.QuickFixUtil
@@ -19,14 +20,80 @@ class FXPropertyConverter : PsiElementBaseIntentionAction() {
     override fun isAvailable(project: Project, editor: Editor?, element: PsiElement): Boolean {
         if (element.isWritable && element.language == KotlinLanguage.INSTANCE) {
             val prop = PsiTreeUtil.getParentOfType(element, KtProperty::class.java)
-            return prop != null && !prop.isLocal
+            if (prop != null) return !prop.isLocal
+
+            val param = PsiTreeUtil.getParentOfType(element, KtParameter::class.java)
+            if (param != null) return true
         }
 
         return false
     }
 
     override fun invoke(project: Project, editor: Editor, element: PsiElement) {
-        val prop = PsiTreeUtil.getParentOfType(element, KtProperty::class.java)!!
+        val prop = PsiTreeUtil.getParentOfType(element, KtProperty::class.java)
+        if (prop != null) {
+            addForProp(prop, project, element)
+        } else {
+            val param = PsiTreeUtil.getParentOfType(element, KtParameter::class.java)
+            if (param != null) addForParam(param, project, element)
+        }
+   }
+
+    private fun addForParam(param: KtParameter, project: Project, element: PsiElement) {
+        val paramName = param.name!!
+        val returnType = QuickFixUtil.getDeclarationReturnType(param)
+
+        object : WriteCommandAction.Simple<String>(project, element.containingFile) {
+            override fun run() {
+                val factory = KtPsiFactory(project)
+                val ktClass = PsiTreeUtil.getParentOfType(element, KtClass::class.java)!!
+                var ktClassBody = PsiTreeUtil.getChildOfType(ktClass, KtClassBody::class.java)
+                if (ktClassBody == null) {
+                    ktClassBody = factory.createEmptyClassBody()
+                    ktClassBody = ktClass.add(ktClassBody) as KtClassBody
+                }
+
+                val typeDecl = "<$returnType>"
+
+                val declaration = factory.createProperty("var $paramName by property$typeDecl()")
+                val propAccessor = factory.createFunction("fun ${paramName}Property() = getProperty(${ktClass.name}::$paramName)")
+
+                if (ktClassBody.children.isEmpty()) {
+                    ktClassBody.addAfter(factory.createNewLine(), ktClassBody)
+                    ktClassBody.addAfter(declaration, ktClassBody.firstChild)
+                    ktClassBody.addAfter(propAccessor, ktClassBody.firstChild)
+                } else {
+                    ktClassBody.addAfter(factory.createNewLine(), ktClassBody.firstChild)
+                    ktClassBody.addAfter(declaration, ktClassBody.firstChild)
+                    ktClassBody.addAfter(propAccessor, ktClassBody.firstChild)
+                }
+
+                addImports()
+
+                while(param.nextSibling?.node?.text == "," || param.nextSibling is PsiWhiteSpace)
+                    param.nextSibling.delete()
+
+                while(param.prevSibling?.node?.text == "," || param.prevSibling is PsiWhiteSpace)
+                    param.prevSibling.delete()
+
+                param.delete()
+            }
+
+            private fun addImports() {
+                // ShortenReferences can't handle tornadofx.getProperty, so imports are added manually
+                val importsFactory = KtImportsFactory(project)
+                val ktFile = PsiTreeUtil.getParentOfType(element, KtFile::class.java)!!
+
+                val imports = ktFile.importList!!.imports
+
+                for (fqName in listOf("tornadofx.property", "tornadofx.getProperty"))
+                    if (imports.find { it.importedFqName.toString() == fqName } == null)
+                        ktFile.importList?.add(importsFactory.createImportDirective(ImportPath(fqName)))
+            }
+        }.execute()
+    }
+
+    private fun addForProp(prop: KtProperty, project: Project, element: PsiElement) {
         val propName = prop.name!!
         val returnType = QuickFixUtil.getDeclarationReturnType(prop)
 
