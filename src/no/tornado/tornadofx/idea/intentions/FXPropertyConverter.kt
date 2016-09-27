c682519c879e2671cbc69ecfd66e7009c7d5392e
Edvin Syse <es@syse.no>
2016-09-27 14:47:40 +0200

    Enhanced fxproperty creation

--- src/no/tornado/tornadofx/idea/intentions/FXPropertyConverter.kt
+++ src/no/tornado/tornadofx/idea/intentions/FXPropertyConverter.kt
@@ -1,12 +1,16 @@
 package no.tornado.tornadofx.idea.intentions
 
+import com.intellij.codeInsight.hint.HintManager
 import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiWhiteSpace
 import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.util.PsiUtil
 import no.tornado.tornadofx.idea.TornadoFXSettings
 import org.jetbrains.kotlin.idea.KotlinLanguage
 import org.jetbrains.kotlin.idea.core.ShortenReferences
@@ -16,6 +20,7 @@ import org.jetbrains.kotlin.js.descriptorUtils.nameIfStandardType
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.resolve.ImportPath
 import org.jetbrains.kotlin.types.KotlinType
+import org.jetbrains.kotlin.types.lowerIfFlexible
 
 class FXPropertyConverter : PsiElementBaseIntentionAction() {
     override fun getText() = "Convert to TornadoFX Property"
@@ -98,7 +103,7 @@ class FXPropertyConverter : PsiElementBaseIntentionAction() {
         object : WriteCommandAction.Simple<String>(project, element.containingFile) {
             override fun run() {
                 val factory = KtPsiFactory(project)
-                val ktClass = PsiTreeUtil.getParentOfType(element, KtClass::class.java)!!
+                val ktClass = PsiTreeUtil.getParentOfType(element, KtClass::class.java)
                 val ktClassBody = PsiTreeUtil.getParentOfType(element, KtClassBody::class.java)!!
 
                 val value = if (prop.hasInitializer() && prop.initializer!!.text != "null") prop.initializer!!.text else ""
@@ -108,10 +113,16 @@ class FXPropertyConverter : PsiElementBaseIntentionAction() {
                 val (declaration, propAccessor) = if (TornadoFXSettings.getInstance().alternativePropertySyntax) {
                     createAlternativePropertyElements(factory, propName, returnType, value, importList)
                 } else {
+                    if (ktClass == null) {
+                        HintManager.getInstance().showErrorHint(FileEditorManager.getInstance(project).selectedTextEditor!!, "Traditional property syntax not available in package level objects. Switch to alternative syntax or try with a class object.")
+                        return
+                    }
                     createPropertyElements(factory, propName, returnType, ktClass, value, importList)
                 }
 
+                ktClassBody.addAfter(factory.createNewLine(), prop)
                 shortenIfNeeded(ktClassBody.addAfter(propAccessor, prop))
+                ktClassBody.addAfter(factory.createWhiteSpace(), prop)
                 shortenIfNeeded(ktClassBody.addAfter(declaration, prop))
 
                 addImports(element, project, importList)
@@ -132,7 +143,7 @@ class FXPropertyConverter : PsiElementBaseIntentionAction() {
                 "Boolean" -> "SimpleBooleanProperty"
                 "Float" -> "SimpleFloatProperty"
                 "String" -> "SimpleStringProperty"
-                else -> "SimpleObjectProperty<$returnType>"
+                else -> "SimpleObjectProperty<${typeName ?: returnType.lowerIfFlexible() ?: returnType}>"
             }
 
             val fullPropName = "javafx.beans.property.$propType"
@@ -146,7 +157,7 @@ class FXPropertyConverter : PsiElementBaseIntentionAction() {
         fun createPropertyElements(factory: KtPsiFactory, paramName: String, returnType: KotlinType, ktClass: KtClass, value: String, importList: MutableSet<String>): Pair<PsiElement, PsiElement> {
             importList.addAll(listOf("tornadofx.property", "tornadofx.getProperty"))
 
-            return factory.createProperty("var $paramName by property<$returnType>($value)") to
+            return factory.createProperty("var $paramName by property<${returnType.nameIfStandardType ?: returnType}>($value)") to
                     factory.createFunction("fun ${paramName}Property() = getProperty(${ktClass.name}::$paramName)")
         }
 
