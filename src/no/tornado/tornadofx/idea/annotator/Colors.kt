83b4d825fc233dbc7723acd6523dcf246f56c6ab
Micha <Micha.Kroeker@gmail.com>
2018-05-05 10:46:31 +0200

    Enhanced the ColorAnnotator
    The following expressions are now supported:
     - c(....)
     - Color.*
     - multi(c(...)|Color.*)
    Colors are now being recognized even out side of the Stylesheet class as long as the project is a tfx project.

---
+++ src/no/tornado/tornadofx/idea/annotator/Colors.kt
@@ -0,0 +1,465 @@
+package no.tornado.tornadofx.idea.annotator
+
+import com.intellij.util.ui.ColorIcon
+import javafx.scene.paint.Color
+
+val defaultFXColorNames = listOf(
+        "TRANSPARENT",
+        "ALICEBLUE",
+        "ANTIQUEWHITE",
+        "AQUA",
+        "AQUAMARINE",
+        "AZURE",
+        "BEIGE",
+        "BISQUE",
+        "BLACK",
+        "BLACK",
+        "BLUE",
+        "BLUEVIOLET",
+        "BROWN",
+        "BURLYWOOD",
+        "CADETBLUE",
+        "CHARTREUSE",
+        "CHOCOLATE",
+        "CORAL",
+        "CORNFLOWERBLUE",
+        "CORNSILK",
+        "CRIMSON",
+        "CYAN",
+        "DARKBLUE",
+        "DARKCYAN",
+        "DARKGOLDENROD",
+        "DARKGRAY",
+        "DARKGREEN",
+        "DARKGREY",
+        "DARKKHAKI",
+        "DARKMAGENTA",
+        "DARKOLIVEGREEN",
+        "DARKORANGE",
+        "DARKORCHID",
+        "DARKRED",
+        "DARKSALMON",
+        "DARKSEAGREEN",
+        "DARKSLATEBLUE",
+        "DARKSLATEGRAY",
+        "DARKSLATEGREY",
+        "DARKTURQUOISE",
+        "DARKVIOLET",
+        "DEEPPINK",
+        "DEEPSKYBLUE",
+        "DIMGRAY",
+        "DIMGREY",
+        "DODGERBLUE",
+        "FIREBRICK",
+        "FLORALWHITE",
+        "FORESTGREEN",
+        "FUCHSIA",
+        "GAINSBORO",
+        "GHOSTWHITE",
+        "GOLD",
+        "GOLDENROD",
+        "GRAY",
+        "GREEN",
+        "GREENYELLOW",
+        "GREY",
+        "HONEYDEW",
+        "HOTPINK",
+        "INDIANRED",
+        "INDIGO",
+        "IVORY",
+        "KHAKI",
+        "LAVENDER",
+        "LAVENDERBLUSH",
+        "LAWNGREEN",
+        "LEMONCHIFFON",
+        "LIGHTBLUE",
+        "LIGHTCORAL",
+        "LIGHTCYAN",
+        "LIGHTGOLDENRODYELLOW",
+        "LIGHTGRAY",
+        "LIGHTGREEN",
+        "LIGHTGREY",
+        "LIGHTPINK",
+        "LIGHTSALMON",
+        "LIGHTSEAGREEN",
+        "LIGHTSKYBLUE",
+        "LIGHTSLATEGRAY",
+        "LIGHTSLATEGREY",
+        "LIGHTSTEELBLUE",
+        "LIGHTYELLOW",
+        "LIME",
+        "LIMEGREEN",
+        "LINEN",
+        "MAGENTA",
+        "MAROON",
+        "MEDIUMAQUAMARINE",
+        "MEDIUMBLUE",
+        "MEDIUMORCHID",
+        "MEDIUMPURPLE",
+        "MEDIUMSEAGREEN",
+        "MEDIUMSLATEBLUE",
+        "MEDIUMSPRINGGREEN",
+        "MEDIUMTURQUOISE",
+        "MEDIUMVIOLETRED",
+        "MIDNIGHTBLUE",
+        "MINTCREAM",
+        "MISTYROSE",
+        "MOCCASIN",
+        "NAVAJOWHITE",
+        "NAVY",
+        "OLDLACE",
+        "OLIVE",
+        "OLIVEDRAB",
+        "ORANGE",
+        "ORANGERED",
+        "ORCHID",
+        "PALEGOLDENROD",
+        "PALEGREEN",
+        "PALETURQUOISE",
+        "PALEVIOLETRED",
+        "PAPAYAWHIP",
+        "PEACHPUFF",
+        "PERU",
+        "PINK",
+        "PLUM",
+        "POWDERBLUE",
+        "PURPLE",
+        "RED",
+        "ROSYBROWN",
+        "ROYALBLUE",
+        "SADDLEBROWN",
+        "SALMON",
+        "SANDYBROWN",
+        "SEAGREEN",
+        "SEASHELL",
+        "SIENNA",
+        "SILVER",
+        "SKYBLUE",
+        "SLATEBLUE",
+        "SLATEGRAY",
+        "SLATEGREY",
+        "SNOW",
+        "SPRINGGREEN",
+        "STEELBLUE",
+        "TAN",
+        "TEAL",
+        "THISTLE",
+        "TOMATO",
+        "TURQUOISE",
+        "VIOLET",
+        "WHEAT",
+        "WHITE",
+        "WHITESMOKE",
+        "YELLOW",
+        "YELLOWGREEN"
+)
+
+val defaultFXColors = listOf(
+        Color.TRANSPARENT,
+        Color.ALICEBLUE,
+        Color.ANTIQUEWHITE,
+        Color.AQUA,
+        Color.AQUAMARINE,
+        Color.AZURE,
+        Color.BEIGE,
+        Color.BISQUE,
+        Color.BLACK,
+        Color.BLACK,
+        Color.BLUE,
+        Color.BLUEVIOLET,
+        Color.BROWN,
+        Color.BURLYWOOD,
+        Color.CADETBLUE,
+        Color.CHARTREUSE,
+        Color.CHOCOLATE,
+        Color.CORAL,
+        Color.CORNFLOWERBLUE,
+        Color.CORNSILK,
+        Color.CRIMSON,
+        Color.CYAN,
+        Color.DARKBLUE,
+        Color.DARKCYAN,
+        Color.DARKGOLDENROD,
+        Color.DARKGRAY,
+        Color.DARKGREEN,
+        Color.DARKGREY,
+        Color.DARKKHAKI,
+        Color.DARKMAGENTA,
+        Color.DARKOLIVEGREEN,
+        Color.DARKORANGE,
+        Color.DARKORCHID,
+        Color.DARKRED,
+        Color.DARKSALMON,
+        Color.DARKSEAGREEN,
+        Color.DARKSLATEBLUE,
+        Color.DARKSLATEGRAY,
+        Color.DARKSLATEGREY,
+        Color.DARKTURQUOISE,
+        Color.DARKVIOLET,
+        Color.DEEPPINK,
+        Color.DEEPSKYBLUE,
+        Color.DIMGRAY,
+        Color.DIMGREY,
+        Color.DODGERBLUE,
+        Color.FIREBRICK,
+        Color.FLORALWHITE,
+        Color.FORESTGREEN,
+        Color.FUCHSIA,
+        Color.GAINSBORO,
+        Color.GHOSTWHITE,
+        Color.GOLD,
+        Color.GOLDENROD,
+        Color.GRAY,
+        Color.GREEN,
+        Color.GREENYELLOW,
+        Color.GREY,
+        Color.HONEYDEW,
+        Color.HOTPINK,
+        Color.INDIANRED,
+        Color.INDIGO,
+        Color.IVORY,
+        Color.KHAKI,
+        Color.LAVENDER,
+        Color.LAVENDERBLUSH,
+        Color.LAWNGREEN,
+        Color.LEMONCHIFFON,
+        Color.LIGHTBLUE,
+        Color.LIGHTCORAL,
+        Color.LIGHTCYAN,
+        Color.LIGHTGOLDENRODYELLOW,
+        Color.LIGHTGRAY,
+        Color.LIGHTGREEN,
+        Color.LIGHTGREY,
+        Color.LIGHTPINK,
+        Color.LIGHTSALMON,
+        Color.LIGHTSEAGREEN,
+        Color.LIGHTSKYBLUE,
+        Color.LIGHTSLATEGRAY,
+        Color.LIGHTSLATEGREY,
+        Color.LIGHTSTEELBLUE,
+        Color.LIGHTYELLOW,
+        Color.LIME,
+        Color.LIMEGREEN,
+        Color.LINEN,
+        Color.MAGENTA,
+        Color.MAROON,
+        Color.MEDIUMAQUAMARINE,
+        Color.MEDIUMBLUE,
+        Color.MEDIUMORCHID,
+        Color.MEDIUMPURPLE,
+        Color.MEDIUMSEAGREEN,
+        Color.MEDIUMSLATEBLUE,
+        Color.MEDIUMSPRINGGREEN,
+        Color.MEDIUMTURQUOISE,
+        Color.MEDIUMVIOLETRED,
+        Color.MIDNIGHTBLUE,
+        Color.MINTCREAM,
+        Color.MISTYROSE,
+        Color.MOCCASIN,
+        Color.NAVAJOWHITE,
+        Color.NAVY,
+        Color.OLDLACE,
+        Color.OLIVE,
+        Color.OLIVEDRAB,
+        Color.ORANGE,
+        Color.ORANGERED,
+        Color.ORCHID,
+        Color.PALEGOLDENROD,
+        Color.PALEGREEN,
+        Color.PALETURQUOISE,
+        Color.PALEVIOLETRED,
+        Color.PAPAYAWHIP,
+        Color.PEACHPUFF,
+        Color.PERU,
+        Color.PINK,
+        Color.PLUM,
+        Color.POWDERBLUE,
+        Color.PURPLE,
+        Color.RED,
+        Color.ROSYBROWN,
+        Color.ROYALBLUE,
+        Color.SADDLEBROWN,
+        Color.SALMON,
+        Color.SANDYBROWN,
+        Color.SEAGREEN,
+        Color.SEASHELL,
+        Color.SIENNA,
+        Color.SILVER,
+        Color.SKYBLUE,
+        Color.SLATEBLUE,
+        Color.SLATEGRAY,
+        Color.SLATEGREY,
+        Color.SNOW,
+        Color.SPRINGGREEN,
+        Color.STEELBLUE,
+        Color.TAN,
+        Color.TEAL,
+        Color.THISTLE,
+        Color.TOMATO,
+        Color.TURQUOISE,
+        Color.VIOLET,
+        Color.WHEAT,
+        Color.WHITE,
+        Color.WHITESMOKE,
+        Color.YELLOW,
+        Color.YELLOWGREEN
+).map { ColorIcon(8, it.toAwtColor()) }
+
+fun String.toColor() = when (this) {
+    "Color.TRANSPARENT" -> Color.TRANSPARENT
+    "Color.ALICEBLUE" -> Color.ALICEBLUE
+    "Color.ANTIQUEWHITE" -> Color.ANTIQUEWHITE
+    "Color.AQUA" -> Color.AQUA
+    "Color.AQUAMARINE" -> Color.AQUAMARINE
+    "Color.AZURE" -> Color.AZURE
+    "Color.BEIGE" -> Color.BEIGE
+    "Color.BISQUE" -> Color.BISQUE
+    "Color.BLACK" -> Color.BLACK
+    "Color.BLANCHEDALMOND" -> Color.BLACK
+    "Color.BLUE" -> Color.BLUE
+    "Color.BLUEVIOLET" -> Color.BLUEVIOLET
+    "Color.BROWN" -> Color.BROWN
+    "Color.BURLYWOOD" -> Color.BURLYWOOD
+    "Color.CADETBLUE" -> Color.CADETBLUE
+    "Color.CHARTREUSE" -> Color.CHARTREUSE
+    "Color.CHOCOLATE" -> Color.CHOCOLATE
+    "Color.CORAL" -> Color.CORAL
+    "Color.CORNFLOWERBLUE" -> Color.CORNFLOWERBLUE
+    "Color.CORNSILK" -> Color.CORNSILK
+    "Color.CRIMSON" -> Color.CRIMSON
+    "Color.CYAN" -> Color.CYAN
+    "Color.DARKBLUE" -> Color.DARKBLUE
+    "Color.DARKCYAN" -> Color.DARKCYAN
+    "Color.DARKGOLDENROD" -> Color.DARKGOLDENROD
+    "Color.DARKGRAY" -> Color.DARKGRAY
+    "Color.DARKGREEN" -> Color.DARKGREEN
+    "Color.DARKGREY" -> Color.DARKGREY
+    "Color.DARKKHAKI" -> Color.DARKKHAKI
+    "Color.DARKMAGENTA" -> Color.DARKMAGENTA
+    "Color.DARKOLIVEGREEN" -> Color.DARKOLIVEGREEN
+    "Color.DARKORANGE" -> Color.DARKORANGE
+    "Color.DARKORCHID" -> Color.DARKORCHID
+    "Color.DARKRED" -> Color.DARKRED
+    "Color.DARKSALMON" -> Color.DARKSALMON
+    "Color.DARKSEAGREEN" -> Color.DARKSEAGREEN
+    "Color.DARKSLATEBLUE" -> Color.DARKSLATEBLUE
+    "Color.DARKSLATEGRAY" -> Color.DARKSLATEGRAY
+    "Color.DARKSLATEGREY" -> Color.DARKSLATEGREY
+    "Color.DARKTURQUOISE" -> Color.DARKTURQUOISE
+    "Color.DARKVIOLET" -> Color.DARKVIOLET
+    "Color.DEEPPINK" -> Color.DEEPPINK
+    "Color.DEEPSKYBLUE" -> Color.DEEPSKYBLUE
+    "Color.DIMGRAY" -> Color.DIMGRAY
+    "Color.DIMGREY" -> Color.DIMGREY
+    "Color.DODGERBLUE" -> Color.DODGERBLUE
+    "Color.FIREBRICK" -> Color.FIREBRICK
+    "Color.FLORALWHITE" -> Color.FLORALWHITE
+    "Color.FORESTGREEN" -> Color.FORESTGREEN
+    "Color.FUCHSIA" -> Color.FUCHSIA
+    "Color.GAINSBORO" -> Color.GAINSBORO
+    "Color.GHOSTWHITE" -> Color.GHOSTWHITE
+    "Color.GOLD" -> Color.GOLD
+    "Color.GOLDENROD" -> Color.GOLDENROD
+    "Color.GRAY" -> Color.GRAY
+    "Color.GREEN" -> Color.GREEN
+    "Color.GREENYELLOW" -> Color.GREENYELLOW
+    "Color.GREY" -> Color.GREY
+    "Color.HONEYDEW" -> Color.HONEYDEW
+    "Color.HOTPINK" -> Color.HOTPINK
+    "Color.INDIANRED" -> Color.INDIANRED
+    "Color.INDIGO" -> Color.INDIGO
+    "Color.IVORY" -> Color.IVORY
+    "Color.KHAKI" -> Color.KHAKI
+    "Color.LAVENDER" -> Color.LAVENDER
+    "Color.LAVENDERBLUSH" -> Color.LAVENDERBLUSH
+    "Color.LAWNGREEN" -> Color.LAWNGREEN
+    "Color.LEMONCHIFFON" -> Color.LEMONCHIFFON
+    "Color.LIGHTBLUE" -> Color.LIGHTBLUE
+    "Color.LIGHTCORAL" -> Color.LIGHTCORAL
+    "Color.LIGHTCYAN" -> Color.LIGHTCYAN
+    "Color.LIGHTGOLDENRODYELLOW" -> Color.LIGHTGOLDENRODYELLOW
+    "Color.LIGHTGRAY" -> Color.LIGHTGRAY
+    "Color.LIGHTGREEN" -> Color.LIGHTGREEN
+    "Color.LIGHTGREY" -> Color.LIGHTGREY
+    "Color.LIGHTPINK" -> Color.LIGHTPINK
+    "Color.LIGHTSALMON" -> Color.LIGHTSALMON
+    "Color.LIGHTSEAGREEN" -> Color.LIGHTSEAGREEN
+    "Color.LIGHTSKYBLUE" -> Color.LIGHTSKYBLUE
+    "Color.LIGHTSLATEGRAY" -> Color.LIGHTSLATEGRAY
+    "Color.LIGHTSLATEGREY" -> Color.LIGHTSLATEGREY
+    "Color.LIGHTSTEELBLUE" -> Color.LIGHTSTEELBLUE
+    "Color.LIGHTYELLOW" -> Color.LIGHTYELLOW
+    "Color.LIME" -> Color.LIME
+    "Color.LIMEGREEN" -> Color.LIMEGREEN
+    "Color.LINEN" -> Color.LINEN
+    "Color.MAGENTA" -> Color.MAGENTA
+    "Color.MAROON" -> Color.MAROON
+    "Color.MEDIUMAQUAMARINE" -> Color.MEDIUMAQUAMARINE
+    "Color.MEDIUMBLUE" -> Color.MEDIUMBLUE
+    "Color.MEDIUMORCHID" -> Color.MEDIUMORCHID
+    "Color.MEDIUMPURPLE" -> Color.MEDIUMPURPLE
+    "Color.MEDIUMSEAGREEN" -> Color.MEDIUMSEAGREEN
+    "Color.MEDIUMSLATEBLUE" -> Color.MEDIUMSLATEBLUE
+    "Color.MEDIUMSPRINGGREEN" -> Color.MEDIUMSPRINGGREEN
+    "Color.MEDIUMTURQUOISE" -> Color.MEDIUMTURQUOISE
+    "Color.MEDIUMVIOLETRED" -> Color.MEDIUMVIOLETRED
+    "Color.MIDNIGHTBLUE" -> Color.MIDNIGHTBLUE
+    "Color.MINTCREAM" -> Color.MINTCREAM
+    "Color.MISTYROSE" -> Color.MISTYROSE
+    "Color.MOCCASIN" -> Color.MOCCASIN
+    "Color.NAVAJOWHITE" -> Color.NAVAJOWHITE
+    "Color.NAVY" -> Color.NAVY
+    "Color.OLDLACE" -> Color.OLDLACE
+    "Color.OLIVE" -> Color.OLIVE
+    "Color.OLIVEDRAB" -> Color.OLIVEDRAB
+    "Color.ORANGE" -> Color.ORANGE
+    "Color.ORANGERED" -> Color.ORANGERED
+    "Color.ORCHID" -> Color.ORCHID
+    "Color.PALEGOLDENROD" -> Color.PALEGOLDENROD
+    "Color.PALEGREEN" -> Color.PALEGREEN
+    "Color.PALETURQUOISE" -> Color.PALETURQUOISE
+    "Color.PALEVIOLETRED" -> Color.PALEVIOLETRED
+    "Color.PAPAYAWHIP" -> Color.PAPAYAWHIP
+    "Color.PEACHPUFF" -> Color.PEACHPUFF
+    "Color.PERU" -> Color.PERU
+    "Color.PINK" -> Color.PINK
+    "Color.PLUM" -> Color.PLUM
+    "Color.POWDERBLUE" -> Color.POWDERBLUE
+    "Color.PURPLE" -> Color.PURPLE
+    "Color.RED" -> Color.RED
+    "Color.ROSYBROWN" -> Color.ROSYBROWN
+    "Color.ROYALBLUE" -> Color.ROYALBLUE
+    "Color.SADDLEBROWN" -> Color.SADDLEBROWN
+    "Color.SALMON" -> Color.SALMON
+    "Color.SANDYBROWN" -> Color.SANDYBROWN
+    "Color.SEAGREEN" -> Color.SEAGREEN
+    "Color.SEASHELL" -> Color.SEASHELL
+    "Color.SIENNA" -> Color.SIENNA
+    "Color.SILVER" -> Color.SILVER
+    "Color.SKYBLUE" -> Color.SKYBLUE
+    "Color.SLATEBLUE" -> Color.SLATEBLUE
+    "Color.SLATEGRAY" -> Color.SLATEGRAY
+    "Color.SLATEGREY" -> Color.SLATEGREY
+    "Color.SNOW" -> Color.SNOW
+    "Color.SPRINGGREEN" -> Color.SPRINGGREEN
+    "Color.STEELBLUE" -> Color.STEELBLUE
+    "Color.TAN" -> Color.TAN
+    "Color.TEAL" -> Color.TEAL
+    "Color.THISTLE" -> Color.THISTLE
+    "Color.TOMATO" -> Color.TOMATO
+    "Color.TURQUOISE" -> Color.TURQUOISE
+    "Color.VIOLET" -> Color.VIOLET
+    "Color.WHEAT" -> Color.WHEAT
+    "Color.WHITE" -> Color.WHITE
+    "Color.WHITESMOKE" -> Color.WHITESMOKE
+    "Color.YELLOW" -> Color.YELLOW
+    "Color.YELLOWGREEN" -> Color.YELLOWGREEN
+    else -> throw IllegalStateException("Color: $this not known!")
+}
+
+fun Color.toAwtColor() = java.awt.Color(//
+        red.toFloat(), //
+        green.toFloat(), //
+        blue.toFloat(), //
+        opacity.toFloat() //
+) //
